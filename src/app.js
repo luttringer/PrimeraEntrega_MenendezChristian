87c58e4ca379c76b25fe5243caa7100f29e28f1b
Christian Menendez Luttringer <luttringerezequiel@gmail.com>
2023-10-28 22:05:42 -0300

    se agrega archivo .env para variables de entorno y se configura en el: puerto, dburl y cookieparser. tambien se installa dotenv

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+//configuracion para utilizar .env
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import exphbs from 'express-handlebars';
 import mongoose from 'mongoose';
@@ -15,12 +19,21 @@ import initializeStrategies from './config/passport.config.js';
 import dictionaryRouter from './router/dictionary.router.js';
 import cors from 'cors';
 
+//variables de entorno
+
+const DB_URL = process.env.DB_URL;
+const ENVPORT = process.env.PORT;
+const COOKIEPARSER =  process.env.COOKIEPARSER;
+
 const app = express();
 //const FileStorage = FileStore(session);      
-const PORT = process.env.PORT||8080;
+const PORT = ENVPORT || 8080;
 const server = app.listen(PORT, ()=>console.log(`esuchando en puerto ${PORT}`));
-const connection = mongoose.connect("mongodb+srv://luttringerezequiel:123@cluster0.obvrjnw.mongodb.net/ecommerce?retryWrites=true&w=majority");
-//const server = http.createServer(app); // Crea el servidor HTTP
+try 
+{
+    const connection = await mongoose.connect(DB_URL, {useNewUrlParser: true,useUnifiedTopology: true});
+    console.log('Conexión a la base de datos exitosa');
+} catch (error) {console.error('Error de conexión a la base de datos:', error);}
 
 
 //configuracion de handlebars
@@ -31,28 +44,15 @@ app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/views`);
 
 //middlewars'
-app.use(cors({origin:['http://localhost:8080']}));
+app.use(cors({origin:['http://localhost:8080'],credentials:true}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static((`${__dirname}/public`)));
-app.use(cookieParser("LECHUZA"));
+app.use(cookieParser(COOKIEPARSER));
 
-
-/*
-app.use(session({
-    store: MongoStore.create({
-        mongoUrl: "mongodb+srv://luttringerezequiel:123@cluster0.obvrjnw.mongodb.net/ecommerce?retryWrites=true&w=majority",
-        ttl:900
-    }),                                            //new FileStorage({path: './sessions', retries:0, ttl:900,reapInterval:10}),
-    secret: 'coderS3cret',
-    resave: false,                   //reescribir session 
-    saveUninitialized: true         //session solo donde se inicialice y utilicen
-}))
-*/
 
 //configuracion de passport, ejecucion
 initializeStrategies();
-//app.use(passport.initialize());
 
 //routes
 app.use('/', views_mRouter);
@@ -60,61 +60,3 @@ app.use('/api/products', product_mRouter);
 app.use('/api/carts', cart_mRouter);
 app.use('/api/sessions', sessionRouter);
 app.use('/api/dictionary', dictionaryRouter);
-
-/*trabajo de cookies*/ 
-/*
-app.get('/home', (req,res)=>{
-    res
-    .cookie('galletaTriste',{name:"Chris:(",lastName:"Menendez:("},{maxAge:10000})
-    .cookie('galletaFeliz', {name:"Chris", lastName:"Menendez"},{signed:true}).send({status:"success", payload:"usuario"});
-})
-
-app.get('/getCookies', (req,res)=>{
-    console.log(req.cookies);
-    res.send(`ok, cookies devueltas. <br><br>cookies: ${req.cookies?.galletaFeliz?.name}`);
-})
-
-app.get('/getSecretCookies', (req,res)=>{
-    console.log(req.signedCookies);
-    if(!req.signedCookies.galletaFeliz) return res.send("rechazado, cookies invalidas");
-    res.send(`ok, cookies devueltas. <br><br>cookies: ${req.signedCookies?.galletaFeliz?.name}`);
-})
-
-app.get('/logout', (req,res)=>{
-    res
-    .clearCookie('galletaTriste')
-    .clearCookie('galletaFeliz').send(`deslogeado`);
-})
-*/
-/*trabajo de sessions*/ 
-/*
-app.get('/access', (req,res)=>
-{
-    if(!req.session.user)
-    {
-        res.send(`No te has logeado aun.`);
-    }else 
-    {
-        res.send(`Bienvenido, ${req.session.user.firstName}`); 
-    }
-})
-
-app.get('/login', (req,res)=>
-{
-    const user = {firstName:"Christian", lastName:"Menendez"};
-    req.session.user = user;
-    res.send("listo login");
-})
-
-app.get('/profile', (req,res)=>
-{
-    res.send({usuario: req.session.user});
-})
-
-app.get('/sessionLogout', (req,res)=>{
-    req.session.destroy(err=>{
-        if(err) return res.status(500).send("fallo el logout")
-        res.send('adios');
-    })
-})
-*/
