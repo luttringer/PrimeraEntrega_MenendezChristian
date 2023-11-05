b40d4b61bfb827f859bd270e3253130aad0195b2
Christian Menendez Luttringer <luttringerezequiel@gmail.com>
2023-11-05 04:48:48 -0300

    se comienza a trabajar y cargar datos de posibles errores en diccionario de errores, se crea carpeta utils y modulo errorDictionary.js para el caso

---
+++ src/utils/errorDictionary.js
@@ -0,0 +1,10 @@
+const errorDictionary = 
+{
+    USER_MAIL_EXIST: "Ya existe un usuario que con el correo indicado. Por favor, utiliza un correo diferente o recupera tu pass.",
+    USER_PASS_WEAK: "Su contraseña es demasiado debil, por favor mejorela.",
+    PRODUCT_INVALID_PRICE: "El precio ingresado es invalido. Por favor, ingresa un numero entero positivo mayor de cero.",
+    PRODUCT_INVALID_STOCK: "El stock ingresado es invalido. Por favor, ingresa un numero entero positivo.",
+    PRODUCT_INVALID_CODE: "Debe ingresar un codigo identificador valido para su producto"
+};
+  
+export default errorDictionary;
