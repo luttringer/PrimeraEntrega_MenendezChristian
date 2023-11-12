ddd05e5f1e2a6e8dcd52a9aff89a00dc5543b889
Christian Menendez Luttringer <luttringerezequiel@gmail.com>
2023-11-12 03:05:46 -0300

    se cambia configuracion de entorno para logger, ahora se setea directamente en attachLogger middleware

--- src/middlewares/attachLogger.js
+++ src/middlewares/attachLogger.js
@@ -1,11 +1,12 @@
 import LoggerService from "../services/LoggerService.js";
 
-const logger = new LoggerService(process.env.LOGGER_ENTORNO);
+//console.log(process.env.LOGGER_ENTORNO);
+const logger = new LoggerService("desarrollo");
 const attachLogger = (req, res, next)=> 
 {
     req.logger = logger.logger;
-    req.logger.http(`${req.method} en ${req.url} = ${new Date().toDateString()}`)
-    next()
+    req.logger.http(`${req.method} en ${req.url} = ${new Date().toDateString()}`);
+    next();
 }
 
 export default attachLogger;
